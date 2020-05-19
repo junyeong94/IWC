5378f953c8acb16edf509176bd4845475872ec92
junyeong94 <64447875+junyeong94@users.noreply.github.com>
2020-05-19 13:22:33 +0900

    Add files via upload

--- js/linktop.js
+++ js/linktop.js
@@ -1,13 +1,20 @@
 $(function(){
-    var headerHeight=$('header').height();
+    var bodyHeight=$('body').height();
+    var footerHeight=$('.footer').height();
+    var scrollHeight=bodyHeight-footerHeight;
     $(document).scroll(function(){
         var docTop=$(document).scrollTop();
-        if(docTop>=80){
-            $('#link_top').addClass('active');
-            $('.header>.content1').slideUp();
+        if(docTop>=scrollHeight){
+            $('#link_top>a').addClass('active');
+            $('#link_top').addClass('active').css('right','35%').css('top','97%');
+            $('.header>.content1').slideUp(300);
+        }else if(docTop>=80){
+            $('#link_top>a').removeClass('active');
+            $('#link_top').addClass('active').css('right','0').css('top','80%');
+            $('.header>.content1').slideUp(300);
         }else{
             $('#link_top').removeClass('active');
-            $('.header>.content1').slideDown();
+            $('.header>.content1').slideDown(300);
         }
     });
 });
